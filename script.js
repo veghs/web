8098c1b8a0493a1cc26211508085e1196c5c87fe
VeghLaszi <136878419+veghs@users.noreply.github.com>
2025-01-13 09:45:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+});
+
+function changeText() {
+    document.getElementById('editable-text').innerText = 'https://discord.gg/GUerNAUKu2';
+}
