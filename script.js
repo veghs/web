9172d97ba4d1375e3e9eb517a4c2db6d93339d30
VeghLaszi <136878419+veghs@users.noreply.github.com>
2025-08-14 18:13:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-});
-
-function changeText() {
-    document.getElementById('editable-text').innerText = 'https://discord.gg/GUerNAUKu2';
-}
